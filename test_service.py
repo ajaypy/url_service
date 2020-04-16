dfcb4bb88e33a7aca114c104d6656f24599bbbf7
ajaypy <ajaygoel@gmail.com>
2020-04-16 02:17:35 -0700

    malware_lookup_service

--- test_service.py
+++ test_service.py
@@ -1,6 +1,5 @@
 import unittest
-
-from x import *
+from url_lookup_service import *
 
 
 class Test_Service_init_in_memory_db(unittest.TestCase):
@@ -10,7 +9,7 @@ class Test_Service_init_in_memory_db(unittest.TestCase):
         self.service.setup()
 
     def test_init(self):
-        assert len(self.service.db_obj.malware_db) == 0
+        assert len(self.service.db_obj.db) == 0
 
 class Test_url_update(unittest.TestCase):
     def setUp(self):
@@ -25,11 +24,11 @@ class Test_url_update(unittest.TestCase):
 
     def test_update_present(self):
         for url in self.updated_urls:
-            assert (url in self.service.db_obj.malware_db), "missing url %s" % url
+            assert (url in self.service.db_obj.db), "missing url %s" % url
 
     def test_update_absent(self):
         for url in self.missing_urls:
-            assert(url not in self.service.db_obj.malware_db), "extra url %s" % url
+            assert(url not in self.service.db_obj.db), "extra url %s" % url
 
 
 class Test_url_lookup(unittest.TestCase):
@@ -41,7 +40,7 @@ class Test_url_lookup(unittest.TestCase):
         self.missing_urls = ['test_url3','test_url4']
 
         for url in self.updated_urls:
-            self.service.db_obj.malware_db.add(url)
+            self.service.db_obj.db.add(url)
 
     def test_lookup_present(self):
         for url in self.updated_urls:
@@ -50,6 +49,76 @@ class Test_url_lookup(unittest.TestCase):
     def test_lookup_absent(self):
         for url in self.missing_urls:
             assert (not self.service.lookup_url(url)), "extra url %s" % url
+
+class Test_url_update_scale(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.service = url_lookup_service()
+        cls.service.setup()
+
+        cls.updated_urls = []
+        for i in range(5000):
+            cls.updated_urls.append('malware_' + str(i))
+        cls.missing_urls = ['missing_url1','missing_url2']
+
+        for url in cls.updated_urls:
+            cls.service.update_url(url)
+
+    def test_update_present(self):
+        for url in self.updated_urls:
+            assert (url in self.service.db_obj.db), "missing url %s" % url
+
+    def test_update_absent(self):
+        for url in self.missing_urls:
+            assert(url not in self.service.db_obj.db), "extra url %s" % url
+
+
+class Test_Service_init_redis_db(unittest.TestCase):
+
+    def setUp(self):
+        self.service = url_lookup_service()
+        self.service.db_info['type'] = 'redis'
+        self.service.db_info['host'] = 'localhost'
+        self.service.db_info['port'] = 6379
+        self.service.setup()
+
+    def test_init(self):
+        db_obj = self.service.db_obj
+        assert (db_obj.db.scard(db_obj.redis_set) == 0), "redis database is not empty"
+
+class Test_url_update_scale_redis(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.service = url_lookup_service()
+        cls.service.db_info['type'] = 'redis'
+        cls.service.db_info['host'] = 'localhost'
+        cls.service.db_info['port'] = 6379
+        cls.service.setup()
+
+        cls.updated_urls = []
+        for i in range(5000):
+            cls.updated_urls.append('malware_' + str(i))
+        cls.missing_urls = ['missing_url1','missing_url2']
+
+        for url in cls.updated_urls:
+            cls.service.update_url(url)
+
+    def test_update_present(self):
+        db_obj = self.service.db_obj
+        for url in self.updated_urls:
+            assert (db_obj.db.sismember(db_obj.redis_set,url)), "missing url %s" % url
+
+    def test_update_absent(self):
+        db_obj = self.service.db_obj
+        for url in self.missing_urls:
+            assert (not db_obj.db.sismember(db_obj.redis_set,url)), "extra url %s" % url
+
+    @classmethod
+    def tearDownClass(cls):
+        db_obj = cls.service.db_obj
+        while (db_obj.db.scard(db_obj.redis_set) != 0):
+            cls.service.db_obj.db.spop(db_obj.redis_set)
 
 if __name__ == '__main__':
     unittest.main()
--- url_lookup_service.py
+++ url_lookup_service.py
@@ -1,50 +1,86 @@
 import optparse
+import redis
+import sys
+import configparser
 
 class url_lookup_service():
 
     def __init__(self,config_file=None):
         self.config_file = config_file
+        self.db_info = {}
+        self.get_config()
 
+    def get_config(self):
+        if self.config_file:
+            config = configparser.ConfigParser()
+            config.read(self.config_file)
+
+            self.db_info['type'] = config['database']['type']
+            if self.db_info['type'] == 'redis':
+                self.db_info['host'] = config['redis']['host']
+                self.db_info['port'] = config['redis']['port']
+        else:
+            self.db_info['type'] = 'in_memory'
 
     def setup(self):
-        self.db_obj = url_malware_db()
+        self.db_obj = url_malware_db(self.db_info)
         self.lookup_obj = url_db_lookup(self.db_obj)
         self.update_obj = url_db_update(self.db_obj)
         self.sync_obj = url_db_syncup(self.db_obj)
-        print (" TODO setup the web framework to receive HTTP requests")
-        print (" TODO setup redis and do initial database syncup")
+        print (" TODO setup web framework for HTTP requests")
+        print (" TODO initial database syncup")
 
     def lookup_url(self,url):
         return self.lookup_obj.get_url_status(url)
 
     def update_url(self,url):
-        self.update_obj.add_malware_url(url)
+        self.update_obj.add_url(url)
         
 class url_malware_db():
-    def __init__(self):
-        self.malware_db = set()
+    def __init__(self,db_info):
+        self.type = db_info['type'] 
+        self.redis_set = 'malware'
+
+        if self.type == 'in_memory':
+            self.db = set()
+        else:
+            try:
+                self.db = redis.Redis(host=db_info['host'],
+                                           port=db_info['port'],
+                                           db=0)
+                self.db.ping()
+            except (ConnectionRefusedError ,redis.exceptions.ConnectionError) as e:
+                print (sys.exc_info()[0])
+                print (sys.exc_info()[1])
+            except Exception as e:
+                print (sys.exc_info()[0])
+                print (sys.exc_info()[1])
+
 
 class url_db_lookup():
-    def __init__(self,db):
-        self.malware_db = db.malware_db
+    def __init__(self,db_obj):
+        self.db_obj = db_obj
 
     def get_url_status(self,url):
-        if url in self.malware_db:
-            return True
-        return False
+        if self.db_obj.type == 'in_memory':
+            return (url in self.db_obj.db)
+        return (self.db_obj.db.sismember(self.db_obj.redis_set, url))
 
 class url_db_update():
 
-    def __init__(self,db):
-        self.malware_db = db.malware_db
+    def __init__(self,db_obj):
+        self.db_obj = db_obj
 
-    def add_malware_url(self,url):
-        self.malware_db.add(url)
+    def add_url(self,url):
+        if self.db_obj.type == 'in_memory':
+            self.db_obj.db.add(url)
+        else:
+            self.db_obj.db.sadd(self.db_obj.redis_set, url)
 
 class url_db_syncup(url_lookup_service):
 
     def __init__(self,db):
-        self.malware_db = db.malware_db
+        self.db = db.db
 
     def sync_malware_url(self,url):
         print ("TODO sync malware sync up database")
@@ -59,6 +95,6 @@ def main():
         if options.conf_file_name == 'None':
            print ("No service configuration supplied")
         url_service = url_lookup_service(options.conf_file_name)
-        url_service.setup_service()
+        url_service.setup()
 if __name__ == '__main__':
     main()
